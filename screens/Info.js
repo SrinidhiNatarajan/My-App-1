4088d49ab2f313a14a525c948978cc416270134f
Srinidhi Natarajan <91948005+SrinidhiNatarajan@users.noreply.github.com>
2023-09-24 11:29:20 +0530

    Update Info.js

--- screens/Info.js
+++ screens/Info.js
@@ -1,1 +1,22 @@
-//DrawerNavigator
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import { getDatabase, ref, onValue } from "firebase/database";
+export default class Information extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Information</Text>
+      </View>
+    );
+  }
+}
